2c49b3f653478099c7f5f81cef1a90dd898cb5c6
Kristian Lalov <lalov.kr@gmail.com>
2023-07-01 02:25:07 +0300

    created class Sprite

--- app.js
+++ app.js
@@ -2,39 +2,13 @@ const canvas = document.getElementById('canvas');
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width;
-const height = canvas.height;
-const hSize = 20;
-const vSize = 20;
-const gridSize = width / hSize;
-
-const snake = {
-  x: 10,
-  y: 10,
-};
-
-function drawGrid() {
-  ctx.strokeStyle = '#999999';
-
-  ctx.beginPath();
-
-  for (let x = 1; x < hSize; x++) {
-    ctx.moveTo(x * gridSize, 0);
-    ctx.lineTo(x * gridSize, height);
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
   }
 
-  for (let y = 1; y < vSize; y++) {
-    ctx.moveTo(0, y * gridSize);
-    ctx.lineTo(width, y * gridSize);
+  draw(){ 
+    ctx.
   }
-  ctx.closePath();
-  ctx.stroke();
 }
-
-function rect(x, y, color) {
-  ctx.fillStyle = color;
-  const snake = ctx.fillRect(x * gridSize, y * gridSize, gridSize, gridSize);
-}
-
-drawGrid();
-rect(snake.x, snake.y, 'orange');
